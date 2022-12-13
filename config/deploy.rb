e17bf2603d355431f6837fe9443143d6a9a663d1
Pooria <pooria@hey.com>
2022-12-13 16:29:41 -0500

    WIP

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "git@github.com:pooriar/pooriar.git"
 
 task :pull_master do
   # Pull the latest code from the Git repository
-  within_release_path do
+  within release_path do
     # execute 'git pull origin master'
     execute 'bundle exec jekyll build'
   end
